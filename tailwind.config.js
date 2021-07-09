6e59520913709dbe7703e04c6fee77b026533c27
Andre Sayej <andresayej@gmail.com>
2021-07-09 16:33:29 +0300

    Tailwind stuff

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    purge: {
+        content: [
+            './components/**/*.{vue,js}',
+            './layouts/**/*.vue',
+            './pages/**/*.vue',
+            './plugins/**/*.{js,ts}',
+            './nuxt.config.{js,ts}',
+            'node_modules/tv-*/dist/tv-*.umd.min.js'
+        ]
+    },
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {}
+    },
+    variants: {
+        extend: {}
+    },
+    plugins: []
+}
